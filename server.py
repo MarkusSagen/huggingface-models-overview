15a6af518cf901b233fbcf2d15a7f34f87ecc107
MarkusSagen <markus.john.sagen@gmail.com>
2021-08-07 22:10:44 +0200

    Route to Vite build dir - Add detailed notes on server and route initialization

--- server.py
+++ server.py
@@ -9,24 +9,52 @@ from starlette.responses import RedirectResponse
 
 app = FastAPI()
 
-app.mount("/client", StaticFiles(directory="client/public", html=True), name="client")
-app.mount("/build", StaticFiles(directory="client/public/build"), name="build")
+# When running the server it will start on `http://localhost:8000`.
+# If we think of our launched application in terms of a folder structure
+# Then in order for the server to find new files, we need to specify/add
+# New paths/folders for where new files should be added from.
+#
+# To add the index.html page, we need to specify the folder where it is located.
+# We specify that the folder contains the main HTML file  we want the users to see
+# However, that file (index.html) tries to find, based on its current location,
+# other files in the "/assets" folder. But since that folder isn't visible to
+# the server, the index file, when served with FastAPI wont know about those
+# either.
+#
+# In order for the HTML file to find them, we also need to mount the "assets"
+# folder. This means that IF any files tries to access any files from "/assets",
+# point them to the MOUNTED folder that we have called "/assets".
+#
+# Thinking of the route as file paths again.
+# - The server is "/"
+# - The first file served is in a new folder "/index"
+# - The assets folder is also a new folder "/assets".
+#
+# If we from the front-end want to make requests from the server,
+# EXAMPLE: get a random number by going to the "/rand" route
+# That would then mean that we would need to go "UP" one directory to access
+# the "/rand" route. That is the reason for why in `App.Svelte` we are calling
+# on `fetch(../rand)`
+
+app.mount("/index", StaticFiles(directory="client/dist", html=True), name="index")
+app.mount("/assets", StaticFiles(directory="client/dist/assets"), name="assets")
 
 
-@app.get('/')
+@app.get("/")
 async def front():
-   return RedirectResponse(url='client')
+    return RedirectResponse(url="index")
+
 
 @app.get("/rand")
 async def hello():
-   return random.randint(0, 100)
+    return random.randint(0, 100)
 
-@app.get('/hello')
+
+@app.get("/hello")
 def read_root():
-    return {'Hello': 'World'}
+    return {"Hello": "World"}
 
-@app.get('/items/{item_id}')
+
+@app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
-
-
